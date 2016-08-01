6f7c26f893d6021905b7df379ae7b44e5e6b2234
MrDoni98 <danil.bartel@gmail.com>
2016-08-01 14:59:54 +0600

    add command and fix

--- src/InvisibleHat/Main.php
+++ src/InvisibleHat/Main.php
@@ -3,38 +3,55 @@
 namespace InvisibleHat;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\entity\Effect;
 use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\item\Item;
 use pocketmine\scheduler\PluginTask;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentEntry;
 
 class Main extends PluginBase implements Listener{
   
   public function onEnable(){
-    $this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this), 5);
+	$this->getServer()->getPluginManager()->registerEvents($this,$this);
+        $this->getServer()->getScheduler()->scheduleRepeatingTask(new HedTask($this), 5);
   }
-}
-
-class Task extends PluginTask{
-  public function __construct(Main $plugin){
-	parent::__construct($plugin);
+  
+  public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
+    	if($cmd->getName() == "ihat"){
+           $player = $this->getServer()->getPlayer($sender->getName());
+           if($player->hasPermission("invisiblehat.cmd")){
+	      $item = Item::get(298, 1, 1);
+	      $item->setCustomName("Шапка-невидимка");
+	      $item->addEnchantment(Enchantment::getEnchantment(0)->setLevel(10));
+	      $sender->getInventory()->addItem($item);
+	      $sender->sendMessage(F::YELLOW."Вы получили Шапку-невидимку");
+	   }else{
+              $sender->sendMessage(F::RED."У Вас нет прав на выполнение этой команды");
+            }
 	}
-
-  public function onRun($tick){
-    $head = $player->getInventory()->getHelmet()
-    foreach($this->getServer()->getOnlinePlayers() as $player){
+  }
+	
+  public function Helmet(){
+	foreach($this->getServer()->getOnlinePlayers() as $player){
             if($player->hasPermission("invisiblehat")){
                $item = Item::get(298, 1);
                $item->setCustomName("Шапка-невидимка");
                $item->addEnchantment(Enchantment::getEnchantment(0)->setLevel(10));
-               if($head == $item){
-                 $effect = Effect::getEffect(14);
-							   $effect->setDuration(999999999999999999999999999);
-						     $effect->setAmplifier($effects->get("INVISIBILTY-AMPLIFIER"));
-							   $effect->setVisible(false);
-							   $player->addEffect($e);
+	       $hed = $player->getInventory()->getHelmet();
+               if($hed->getCustomName() == $item->getCustomName()){
+		  if($hed->getEnchantment(0)){
+		     if($hed->getId() == 298){
+			$player->sendTip(F::BLUE."Вы невидимы");
+		        $player->addEffect(Effect::getEffect(14)->setDuration(15)->setAmplifier(1)->setVisible(false));
+		     }
+		  }
                }
             }
-    }
+       }
   }
-
 }
